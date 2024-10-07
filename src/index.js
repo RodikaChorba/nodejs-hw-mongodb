6ff6512b8253628aeb1cd78aaec9935f3fdabc1f
Rodika Chorba <158561902+RodikaChorba@users.noreply.github.com>
2024-10-07 20:24:29 +0200

    change env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { setupServer } from './server.js';
 const bootstrap = async () => {
   await initMongoConnection();
   setupServer();
-  // const PORT = process.env.PORT || 3000;
 };
 
 bootstrap();
